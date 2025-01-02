259420287d1017b0744f1334e9c1bc9c8b33fde2
Pavel <keruzel@protonmail.com>
2025-01-02 14:06:01 +0300

    Create Main.cpp
    Clean main func

---
+++ Main.cpp
@@ -0,0 +1,27 @@
+#include "../headers/Dependencies.hpp"
+#include "../headers/Defenitions.hpp"
+#include "../headers/FunctionInfo.hpp"
+
+int main() {
+    // std::vector<FunctionInfo> functionInfo = {
+    //     FunctionInfo("MessageBoxW", "User32.dll")    
+    // };
+
+    // resolveFunctions(functionInfo);
+
+    // try {
+    //     resolveFunctions(functionInfo);
+    //     std::cout << "Functions resolved successfully." << std::endl;
+    // } catch (const std::exception& e) {
+    //     std::cerr << "Error: " << e.what() << std::endl;
+    //     return 1;
+    // }
+    
+    // FunctionInfo* messageBoxInfo = findFunctionByName(functionInfo, "MessageBoxW");
+    // if (messageBoxInfo != nullptr) {
+    //     std::cout << messageBoxInfo->getFunctionName() << std::endl;
+    //     std::cout << messageBoxInfo->getFunctionPointer() << std::endl;
+    // }
+
+    return 0;
+}
